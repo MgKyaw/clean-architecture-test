3fc3e4efca26abc6b9f09c26595174cafcc4694a
MgKyaw <mgkyaw20112@gmail.com>
2024-08-12 23:31:06 +0700

    feat: Add Movie Class

---
+++ Movies.Core/Entities/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace Movies.Core.Entities;
+
+public class Movie: Entity
+{
+    public string MovieName { get; set; }
+    public string DirectorName { get; set;}
+    public string ReleasedYear { get; set;}
+}
