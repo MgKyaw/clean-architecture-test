f2e58a0ff78d1da5a48c782521c4dd968c3c2ed9
MgKyaw <mgkyaw20112@gmail.com>
2024-08-13 15:22:36 +0700

    feat: Add MovieContextSeed

---
+++ Movies.Infrastructure/Data/MovieContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure;
+
+public class MovieContextSeed
+{
+    public static async Task SeedAsync(MovieContext movieContext, ILoggerFactory loggerFactory, int? retry = 0)
+    {
+        int maxRetryCount = 3;
+        int retryForAvailability = retry ?? 0;
+
+        try
+        {
+            await movieContext.Database.EnsureCreatedAsync();
+
+            if (retryForAvailability < maxRetryCount)
+            {
+                if (!movieContext.Movies.Any())
+                {
+                    movieContext.Movies.AddRange(GetMovies());
+                    await movieContext.SaveChangesAsync();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            retryForAvailability++;
+            var log = loggerFactory.CreateLogger<MovieContextSeed>();
+            log.LogError($"Exception occurs while connecting: {ex.Message}");
+            await SeedAsync(movieContext, loggerFactory, retryForAvailability);
+        }
+    }
+
+    private static IEnumerable<Movie> GetMovies()
+    {
+        return new List<Movie>()
+        {
+            new Movie { MovieName = "Avatar", DirectorName = "James Cameron", ReleaseYear = "2009"},
+            new Movie { MovieName = "Titanic", DirectorName = "James Cameron", ReleaseYear = "1997"},
+            new Movie { MovieName = "Die Another Day", DirectorName = "Lee Tamahori", ReleaseYear = "2002"},
+            new Movie { MovieName = "Godzilla", DirectorName = "Gareth Edwards", ReleaseYear = "2014" }
+        };
+    }
+}
