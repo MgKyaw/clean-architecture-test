871f75d800812eb916f24386d6137beffe40f675
MgKyaw <mgkyaw20112@gmail.com>
2024-08-13 15:22:15 +0700

    fix: Rename ReleasedYear to ReleaseYear

--- Movies.Core/Entities/Movie.cs
+++ Movies.Core/Entities/Movie.cs
@@ -6,5 +6,5 @@ public class Movie: Entity
 {
     public string MovieName { get; set; }
     public string DirectorName { get; set;}
-    public string ReleasedYear { get; set;}
+    public string ReleaseYear { get; set;}
 }
