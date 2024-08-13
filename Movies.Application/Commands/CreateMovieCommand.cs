2b1836a06407bd1b62b3520fcb188259eeaaa94a
MgKyaw <mgkyaw20112@gmail.com>
2024-08-13 18:23:05 +0700

    feat: Modify namespace of CreateMovieCommand

--- Movies.Application/Commands/CreateMovieCommand.cs
+++ Movies.Application/Commands/CreateMovieCommand.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
 using Movies.Application.Responses;
 
-namespace Movies.Application;
+namespace Movies.Application.Commands;
 
 public class CreateMovieCommand: IRequest<MovieResponse>
 {
