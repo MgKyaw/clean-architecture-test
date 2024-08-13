78b28aacdc6b509267cd60df308659299ed6898b
MgKyaw <mgkyaw20112@gmail.com>
2024-08-13 16:09:49 +0700

    feat: Add MovieRepository.cs

---
+++ Movies.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Repositories;
+using Movies.Infrastructure.Repositories.Base;
+using Movies.Infrastructure.Data;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure;
+
+public class MovieRepository : Repository<Movie>, IMovieRepository
+{
+    public MovieRepository(MovieContext movieContext) : base(movieContext)
+    {
+    }
+
+    public async Task<IEnumerable<Movie>> GetMoviesByDirectorName(string directorName)
+    {
+        return await _movieContext.Movies
+                     .Where(m => m.DirectorName == directorName)
+                     .ToListAsync();
+    }
+}
