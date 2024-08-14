92cc6f7e96275cdec24738137b779a87d26f80b5
MgKyaw <mgkyaw20112@gmail.com>
2024-08-14 16:26:48 +0700

    feat: Modify Program.cs

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,25 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Movies.Infrastructure.Data;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+namespace Movies.API;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        await CreateAndSeedDb(host);
+        host.Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+    private static async Task CreateAndSeedDb(IHost host)
+    {
+        using(var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var movieContext = services.GetRequiredService<MovieContext>();
+                await MovieContextSeed.SeedAsync(movieContext, loggerFactory);
+            }
+            catch(Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError($"Exception occured in API: {ex.Message}");
+            }
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
